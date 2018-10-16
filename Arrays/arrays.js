8d13f968015192296bc667be192676a38374c709
Jeffery Patton <jefferypatton@jeffpattonsmbp.lan>
2018-10-16 10:34:13 -0400

    Arrays

--- Arrays/arrays.js
+++ Arrays/arrays.js
@@ -28,16 +28,46 @@ function rotateArray(arr, n) {
   //return [...arr.slice(arr.length - n), ...arr.slice(0, arr.length-n)]; // 1 line
 }
 
-// Challenge #2: Find sum of 3 largest values in an array
+// Challenge #2: Find sum of 3 largest values in an array - Assume that the array always has at least 3 items
 
 function findSumOfThree(arr) {
-  let large = 0;
-  let mid = 0;
-  let small = 0;
+  // Set the values to -Infinity to ensure that no data too small gets passed
+  let large = -Infinity;
+  let mid = -Infinity;
+  let small = -Infinity;
 
   for (let i = 0; i < arr.length; i++) {
-    // Replace largest
-    // Replace mid
-    // Replace small
+    if (arr[i] > large) {
+      // Replace largest
+      small = mid; // Get rid of smallest
+      mid = large; //
+      large = arr[i];
+    } else if (arr[i] > mid) {
+      // Replace mid
+      small = mid;
+      mid = arr[i];
+    } else if (arr[i] > small) {
+      // Replace small
+      small = arr[i];
+    }
   }
+
+  return large + mid + small;
 }
+
+// Challenge 3: Quarantine elements
+const arr = [1, 2, 3, 4, 5];
+const arr2 = [1, 2, 2, 3, 4, 5];
+function quarantineElements(arr, badElement) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === 'Triage') break;
+    if (arr[i] === badElement) {
+      if (!arr.includes('Triage')) arr.push('Triage'); // Add 'Triage' to the end if it isn't already within the array
+      arr.splice(i, 1); // Remove the bad item
+      arr.push(badElement); // Add the bad item to the end
+      i--; // Decrement i to make sure we don't skip this position the next time around.
+    }
+  }
+  return arr;
+}
+quarantineElements(arr, 2);
