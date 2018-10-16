4cd50b6b2472665f2b7e229dcf5e3ef5be397ddd
Jeffery Patton <jefferypatton@jeffpattonsmbp.lan>
2018-10-16 13:18:18 -0400

    arr5

--- Arrays/arrays.js
+++ Arrays/arrays.js
@@ -87,3 +87,72 @@ function hasDuplicate(arr) {
   }
   return false; // If we have not foun any duplicates, return false.
 }
+
+// Add firstIndex for no sorting and no additional vars
+
+// Add sort for no additional vars
+
+// Problem 5:
+// 4 Solutions
+// Basic
+// Sort + walkthrough
+function findMissing(arr) {
+  arr.sort((a, b) => a - b);
+
+  for (let i = 0; i < arr.length + 1; i++) {
+    if (arr[i] !== i) {
+      return i;
+    }
+  }
+}
+
+// Two maps - Loop through complete map and see which one is missing
+function findMissing(arr) {
+  let incompleteMap = {};
+  let completeMap = {};
+
+  for (let i = 0; i < arr.length + 1; i++) {
+    completeMap[i] = true;
+  }
+  for (let i = 0; i < arr.length + 1; i++) {
+    incompleteMap[arr[i]] = true;
+  }
+
+  for (let key in completeMap) {
+    if (incompleteMap[key] === undefined) {
+      return Number(key);
+    }
+  }
+}
+
+// Bonus
+// Includes
+/*
+  Why it performs worse:
+    Includes has to do a search on the items, so you're looking at each item multiple times as you go through the array
+*/
+function findMissing(arr) {
+  // Loop through all items that SHOULD be within the array
+  for (let i = 0; i <= arr.length; i++) {
+    if (!arr.includes(i)) {
+      // If the array does NOT include the current number
+      return i; // Return the current number
+    }
+  }
+}
+// Difference of sums
+// Optimal performance: We look at each item exactly once
+function findMissing(arr) {
+  let completeSum = 0;
+  let incompleteSum = 0;
+  // Obtain the sum of what the complete sequence WOULD be
+  for (let i = 0; i <= arr.length; i++) {
+    completeSum += i;
+  }
+  // Obtain the sum of what the sequence ACTUALLY is
+  for (let i = 0; i < arr.length; i++) {
+    incompleteSum += arr[i];
+  }
+  // Return the difference
+  return completeSum - incompleteSum;
+}
