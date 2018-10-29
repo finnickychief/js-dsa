1e85baab7ad3747bcf097870b9c28b86b75905d2
Jeffery Patton <jefferypatton@jeffpattonsmbp.lan>
2018-10-29 10:43:29 -0400

    monday Starter

---
+++ Extra/workerManager.js
@@ -0,0 +1,82 @@
+let dataArray = [
+  {
+    managerWork: 20,
+    workerWork: 8,
+    workSites: [
+      {
+        name: 'Site1',
+        workToComplete: 20
+      },
+      {
+        name: 'Site2',
+        workToComplete: 62
+      }
+    ]
+  },
+  {
+    managerWork: 2,
+    workerWork: 10,
+    workSites: [
+      {
+        name: 'Site1',
+        workToComplete: 30
+      },
+      {
+        name: 'Site2',
+        workToComplete: 62
+      }
+    ]
+  },
+  {
+    managerWork: 12,
+    workerWork: 7,
+    workSites: [
+      {
+        name: 'Site1',
+        workToComplete: 11
+      },
+      {
+        name: 'Site2',
+        workToComplete: 21
+      }
+    ]
+  },
+  {
+    managerWork: 20,
+    workerWork: 8,
+    workSites: [
+      {
+        name: 'Site1',
+        workToComplete: 21
+      },
+      {
+        name: 'Site2',
+        workToComplete: 62
+      },
+      {
+        name: 'Site3',
+        workToComplete: 18
+      },
+      {
+        name: 'Site4',
+        workToComplete: 42
+      }
+    ]
+  }
+];
+
+// Answers are as follows:
+/*
+  Day 1: 
+    Workers required: 8, Manager placement: Site1
+
+  Day 2:
+    Workers required: 9, Manager placement: Site2
+
+  Day 3:
+    Workers required: 3, Manager placement: Site1
+
+  Day 4:
+    Workers required: 17, Manager placement: Site3 
+
+*/
